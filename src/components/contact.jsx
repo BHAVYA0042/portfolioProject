6437506bbb8fe3838c1e2baece31b6cce0c12f73
Bhavya Gupta <bhavyag4200@gmail.com>
2023-03-23 14:39:45 +0530

    Contact Commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,8 +5,21 @@ import "../css/contact.scss";
 
 export default function Contact() {
   const [data,setData]=useState({});
-
-
+  const [formData,setFormData]=useState({
+    name:"",
+    email:"",
+    subject:"",
+    body:""
+  });
+  const [mailString,setMailString]=useState("");
+  function handleChange(e){
+    const {name,value}=e.target;
+    setFormData({
+      ...formData,
+      [name]:value
+    })
+    setMailString(`mailto:${data.email}?subject=${formData.subject}&body=${formData.body}`)
+  }
   useEffect(()=>{
       let cat="contactMe";
       dataServiceObj.getCategory(cat).then(resData=>{
@@ -17,7 +30,7 @@ export default function Contact() {
     <div className='contact'>
       <div className="contact_left">
         <section>
-          <a href="mailto:">
+          <a href={`mailto:${data.email}`}>
             <i class="bi bi-envelope"></i>
           </a>
           <p>{data.email}</p>
@@ -37,11 +50,11 @@ export default function Contact() {
       </div>
       <div className='contact_right'>
         <form>
-          <input type="text" placeholder='Name'/>
-          <input type="text" placeholder='Email'/>
-          <input type="text" placeholder='Subject'/>
-          <textarea placeholder='Message'/>
-          <button>Send Message</button>
+          <input type="text" placeholder='Name' value={formData.name} name="name" onChange={handleChange}/>
+          <input type="text" placeholder='Email' value={formData.email} name="email" onChange={handleChange}/>
+          <input type="text" placeholder='Subject' value={formData.subject} name="subject" onChange={handleChange}/>
+          <textarea placeholder='Message' value={formData.body} name="body" onChange={handleChange}/>
+          <a href={mailString}>Send Message</a>
 
         </form>
       </div>
